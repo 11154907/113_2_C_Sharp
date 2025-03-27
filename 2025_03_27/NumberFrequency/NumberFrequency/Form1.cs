3dfc5399303aef7afa5067fd88d2dcbfbc77ec5f
a113221027 <a113221027@mail.shu.edu.tw>
2025-03-27 13:02:53 +0800

    2025_03_27

---
+++ 2025_03_27/NumberFrequency/NumberFrequency/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberFrequency
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const int SIZE = 1000;
+            int num;
+            double frequency;
+            Random random = new Random();
+            int[] numbers = new int[SIZE];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(1, 101);
+            }
+
+            if(int.TryParse(textBox1.Text, out num))
+            {
+                frequency = frequencyOfNumber(numbers, num)/SIZE;
+                MessageBox.Show("數字" + num + "出現的機率為" + frequency.ToString("P"));
+            }
+            else
+            {
+                MessageBox.Show("型態錯誤 : 請輸入整數!");
+            }
+
+
+        }
+        private int frequencyOfNumber(int[] numbers, int num)
+        {
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == num)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
